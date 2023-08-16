0fa75bb184ba02ff0b5cbdbf4fe724164d07ac4a
wonseop <wonseop9427@gmail.com>
2023-08-16 17:54:37 +0900

    화면에 글자 출력하기 print()과 println()

---
+++ Hello/src/Hello/Ex2_1.java
@@ -0,0 +1,11 @@
+package Hello;
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World"); //화면에 Hello, world를 출력하고 줄 바꿈을 한다.
+		System.out.print("Hello"); //화면에 Hello를 출력하고 줄 바꿈을 안한다.
+		System.out.println("World"); // 화면에 World를 출력하고 줄바꿈 한다.
+	}
+
+}
---
+++ Hello/src/Hello/Ex2_2.java
@@ -0,0 +1,11 @@
+package Hello;
+
+public class Ex2_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World"); // 화면에 Hello, World가 출력된다.
+		System.out.print("3+5 = "); //화면에 3+5 = 를 출력하고 줄 바꿈을 안한다.
+		System.out.println(3+5); // 화면에 8이 출력된다.
+	}
+
+}
